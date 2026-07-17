894b73f413a015ae376cce4ad324577badc7d26a
Jason Christopherson <jchristopherson@hotmail.com>
2026-07-16 19:42:02 -0500

    Clean up

--- configure/template.cmake
+++ configure/template.cmake
@@ -1,10 +1,8 @@
 @PACKAGE_INIT@
 
-if (NOT BUILD_SHARED_LIBS)
-  include(CMakeFindDependencyMacro)
-  find_dependency(ferror QUIET)
-  find_dependency(linalg QUIET)
-endif()
+include(CMakeFindDependencyMacro)
+find_dependency(ferror QUIET)
+find_dependency(linalg QUIET)
 
 if(NOT TARGET "@PROJECT_NAME@::@PROJECT_NAME@")
   include("${CMAKE_CURRENT_LIST_DIR}/@PROJECT_NAME@-targets.cmake")
