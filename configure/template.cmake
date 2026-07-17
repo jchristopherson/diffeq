ca0d64203fc69305cfb797abfb5ad4489779ff71
Jason Christopherson <jchristopherson@hotmail.com>
2026-07-17 06:09:03 -0500

    V1.1.5 (#11)
    * Update CI
    
    * Clean up build scripts
    
    * Clean up
    
    * Clean up
    
    * Clean up
    
    * Update version requirements

--- configure/template.cmake
+++ configure/template.cmake
@@ -1,5 +1,9 @@
 @PACKAGE_INIT@
 
+include(CMakeFindDependencyMacro)
+find_dependency(ferror QUIET)
+find_dependency(linalg QUIET)
+
 if(NOT TARGET "@PROJECT_NAME@::@PROJECT_NAME@")
   include("${CMAKE_CURRENT_LIST_DIR}/@PROJECT_NAME@-targets.cmake")
 endif()
